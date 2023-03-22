a42cb4937b3d351b97fbf7c5d02dc7b77bf66418
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-03-22 10:51:31 +0545

    Suggested movies added in detail page

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 
 const Details = () => {
   const [movie,setMovie]=useState({});
+  const [suggestion,setSuggestion]=useState([]);
   const {id}=useParams();
-  console.log(id);
+  // console.log(id);
   // let genres=movie.genres;
   // console.log(genres.join(","));
   // const location=useLocation();
@@ -14,7 +15,14 @@ const Details = () => {
 //  console.log(movie.genres.join(",")) 
  useEffect(()=>{
     getApi();
-  },[])
+    getSuggestedMovies();
+  },[id])
+
+  function getSuggestedMovies(){
+    const promise=fetch(`https://yts.mx/api/v2/movie_suggestions.json?movie_id=${id}`);
+    promise.then(res=>res.json())
+    .then(data=>setSuggestion(data.data.movies));
+  }
 
 async function getApi(){
   try{
@@ -40,8 +48,27 @@ async function getApi(){
       <p>Genres:{(movie.genres)}</p>
       <p style={{marginTop:"10px"}}>Description</p>
       <p style={{marginTop:"20px"}}>{movie.description_full}</p>
-      {/* <p>Hello</p> */}
-      {/* </div> */}
+      <div className='movies-suggestion'>
+        <p className='title-movies-suggestion'>Movie Suggestion</p>
+        {/* <div className='content-box'>
+          
+        </div>
+        <div className='content-box'>
+          
+        </div>
+        <div className='content-box'>
+          
+        </div> */}
+        {suggestion.map(({title,medium_cover_image,id})=>(
+          <div className='suggestion-box' key={id}>
+          <img  src={medium_cover_image} alt={title}/>
+          <p >{title}</p>
+          <Link to={`../details/${id}`}  className='btn'>Details</Link>
+          {/* <Link to='/details' className='btn'><button className='btn' onClick=''>Details</button></Link> */}
+          </div>
+        ))}
+        {/* <p></p> */}
+      </div>
     </div>
   )
 }
